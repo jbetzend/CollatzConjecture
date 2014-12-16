c0669c949430b9893218738926838cee3754101a
Jonas Betzendahl <jbetzend@techfak.uni-bielefeld.de>
2014-12-16 09:09:43 +0100

    Fixed IO issuses (old files now get deleted and rewritten) and introduced multithreading

--- Main.hs
+++ Main.hs
@@ -1,20 +1,43 @@
 module Main where
 
+{-- LOL, imports --}
+
 import           System.IO
-import qualified System.IO.Strict   as SIO
+import           System.IO.Unsafe
 import           System.Directory
 import           System.Environment
 
-import Control.Monad
-import Control.Concurrent
-import Control.Concurrent.STM
+import           Control.Monad
+import           Control.Concurrent
+import           Control.Concurrent.STM
 
 import           Data.Binary
 import qualified Data.List                  as DL
 import qualified Data.Map.Strict            as DM
 import qualified Data.ByteString.Lazy.Char8 as BL
 
+-- TODO: Apparently, "Newtype" is coding convention
 type TermMap = DM.Map Integer Bool
+
+{-- Concurrency foo. Don't blame me for unsafePerformIO, docs for Control.Concurrent told me to do it! --}
+
+children :: MVar [MVar ()]
+children = unsafePerformIO (newMVar [])
+
+waitForChildren :: IO ()
+waitForChildren = do
+  cs <- takeMVar children
+  case cs of
+       []   ->    return ()
+       m:ms -> do putMVar children ms
+                  takeMVar m
+                  waitForChildren
+
+forkChild :: IO () -> IO ThreadId
+forkChild io = do mvar   <- newEmptyMVar
+                  childs <- takeMVar children
+                  putMVar children (mvar:childs)
+                  forkFinally io (\_ -> putMVar mvar ())
 
 dynCollatz :: TVar TermMap -> Integer -> IO ()
 dynCollatz tm n0 = do accum <- readTVarIO tm
@@ -33,6 +56,8 @@ dynCollatz tm n0 = do accum <- readTVarIO tm
     recInsert :: [Integer] -> Bool -> TermMap -> TermMap
     recInsert is b m = DL.foldr (\n -> DM.insert n b) m is
 
+{-- main thread --}
+
 main :: IO ()
 main = do hSetBuffering stdout NoBuffering
           args <- getArgs
@@ -45,15 +70,23 @@ main = do hSetBuffering stdout NoBuffering
                    else do encodeFile "collatzMap.prog" (1                          :: Integer)
                            encodeFile "collatzMap.dat"  ((DM.fromList [(1, True)])  :: TermMap)
 
-          f <- SIO.readFile "collatzMap.dat"
-          let savedMap = decode (BL.pack f) :: TermMap
-          
-          g <- SIO.readFile "collatzMap.prog"
-          let prevsize = decode (BL.pack g) :: Integer
+          savedMap <- decodeFile "collatzMap.dat"  :: IO TermMap
+          prevsize <- decodeFile "collatzMap.prog" :: IO Integer
 
           terminateMap <- atomically $ newTVar savedMap
-          mapM_ forkIO [dynCollatz terminateMap n | n <- [prevsize .. inp]]
-          encodeFile "collatzMap.prog" inp
+
+          putStrLn "Now spawning..."
+
+          mapM_ forkIO [dynCollatz terminateMap n | n <- [prevsize .. inp]] -- (should be prevsize + inp)
+
+          removeFile "collatzMap.dat"  -- old files no longer needed!
+          removeFile "collatzMap.prog"   
+
+          putStrLn "Now waiting..."
+          waitForChildren
+
           newMap <- readTVarIO terminateMap
+          
+          encodeFile "collatzMap.prog" inp
           encodeFile "collatzMap.dat"  newMap
           putStrLn "All done!"
