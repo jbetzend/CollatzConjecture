4b760522cd5d4217141ff915970bf76c7effb10e
Jonas Betzendahl <jbetzend@techfak.uni-bielefeld.de>
2014-12-26 00:03:42 +0100

    Switched to limit and HashMap, problems with determinism

--- Main.hs
+++ Main.hs
@@ -18,7 +18,14 @@ import qualified Data.HashSet               as HS
 import qualified Data.List                  as DL
 import qualified Data.ByteString.Lazy.Char8 as BL
 
-{-- Type Foo. --}
+{-- Type Foo and associates --}
+
+type Results = (Integer, HashSet Integer)
+
+unify :: Results -> Results
+unify res@(n0, hs)
+  | (HS.member (n0 + 1) hs) = unify (n0 + 1, HS.delete (n0 + 1) hs)
+  | otherwise               = res
 
 instance (Eq t, Hashable t, Binary t) => Binary (HashSet t) where
   put = (put . HS.toList)
@@ -47,18 +54,22 @@ forkChild io = do mvar   <- newEmptyMVar
 
 {-- Collatz Foo --}
 
-results :: TVar (HashSet Integer)
+results :: TVar Results
 results = unsafePerformIO $ newTVarIO (undefined)
 
 dynCollatz :: Integer -> IO ()
-dynCollatz n0 = do accum <- readTVarIO results
-                   let (is, terminates) = coll (next n0) n0 accum 
-                   if terminates then atomically $ writeTVar results $ recInsert is accum
+dynCollatz n0 = do results_tvar <- readTVarIO results
+                   let (is, terminates) = coll n0 n0 False results_tvar 
+                   if terminates then atomically $ writeTVar results $ (fst results_tvar, recInsert is (snd results_tvar))
                                  else putStrLn   $ "Loop @ " ++ show n0
   where
-    coll :: Integer -> Integer -> HashSet Integer -> ([Integer], Bool)
-    coll n i set = if n == i then ([n], False) else case HS.member n set of
-      True -> ([n], True) ; False -> let (is, c) = coll (next n) i set in (n:is, c)
+    coll :: Integer -> Integer -> Bool -> Results -> ([Integer], Bool)
+    coll n i visited res@(lim, set)
+      | (n <= lim) = ([n], True)
+      | (n == i)   = if visited then ([n], False)
+                                else let (is, c) = coll (next n) i True res in (n:is, c)
+      | otherwise  = if HS.member n set then ([n], True)
+                                        else let (is, c) = coll (next n) i visited res in (n:is, c)
 
     next :: Integer -> Integer
     next n = case even n of False -> 3*n + 1 ; True -> n `div` 2
@@ -73,29 +84,26 @@ main = do hSetBuffering stdout NoBuffering -- standard
 
           args <- getArgs
           let step  = (read (args !! 0)) :: Integer
-          exsts1    <- doesFileExist "collatzMap.dat"
-          exsts2    <- doesFileExist "collatzMap.prog"
-          let exsts  = exsts1 && exsts2          
+
+          exsts <- doesFileExist "collatzMap.dat"
           if exsts then return () -- we assume these files exists. If they don't, we have to create them now
-                   else do encodeFile "collatzMap.prog" (1                 ::         Integer)
-                           encodeFile "collatzMap.dat"  ((HS.fromList [1]) :: HashSet Integer)
+                   else encodeFile "collatzMap.dat" ((1, (HS.fromList [1])) :: Results)
 
-          savedSet <- decodeFile "collatzMap.dat"  :: IO (HashSet Integer)     -- read progress from disk
-          prevsize <- decodeFile "collatzMap.prog" :: IO          Integer
+          savedResults <- decodeFile "collatzMap.dat"  :: IO Results     -- read progress from disk
+          atomically $ writeTVar results savedResults
+          
+          let prevSize = fst savedResults
+          let newSize  = prevSize + step
 
-          atomically $ writeTVar results savedSet
-          let newSize = prevsize + step
-
-          mapM_ forkIO [dynCollatz n | n <- [prevsize .. newSize]]
+          mapM_ (forkIO . dynCollatz) [prevSize .. newSize]
 
           removeFile "collatzMap.dat"  -- old files no longer needed!
-          removeFile "collatzMap.prog"   
-
           waitForChildren -- Wait for all children to terminate
 
           newSet <- readTVarIO results
-          
-          encodeFile "collatzMap.prog" newSize
-          encodeFile "collatzMap.dat"  newSet
+          let final = unify newSet
+          encodeFile "collatzMap.dat" final
 
-          putStrLn $ "Finished calculating up to " ++  show newSize ++ " :)"
+          print final
+          
+          putStrLn $ "Finished calculating up to " ++ show newSize ++ " :)"
