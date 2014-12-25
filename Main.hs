a6ec80a9427ee5ea2a1323194195c05710909ee0
Jonas Betzendahl <jbetzend@techfak.uni-bielefeld.de>
2014-12-25 22:30:13 +0100

    Switched to HashSets. Still not enough.

--- Main.hs
+++ Main.hs
@@ -12,10 +12,18 @@ import           Control.Concurrent
 import           Control.Concurrent.STM
 
 import           Data.Binary
-import           Data.Set (Set)
-import qualified Data.Set                   as DS
+import           Data.Hashable
+import           Data.HashSet (HashSet)
+import qualified Data.HashSet               as HS
 import qualified Data.List                  as DL
 import qualified Data.ByteString.Lazy.Char8 as BL
+
+{-- Type Foo. --}
+
+instance (Eq t, Hashable t, Binary t) => Binary (HashSet t) where
+  put = (put . HS.toList)
+  get = do hs <- get
+           return $ HS.fromList hs
 
 {-- Concurrency foo. Don't blame me for unsafePerformIO, docs for Control.Concurrent told me to do it! --}
 
@@ -39,7 +47,7 @@ forkChild io = do mvar   <- newEmptyMVar
 
 {-- Collatz Foo --}
 
-results :: TVar (Set Integer)
+results :: TVar (HashSet Integer)
 results = unsafePerformIO $ newTVarIO (undefined)
 
 dynCollatz :: Integer -> IO ()
@@ -48,15 +56,15 @@ dynCollatz n0 = do accum <- readTVarIO results
                    if terminates then atomically $ writeTVar results $ recInsert is accum
                                  else putStrLn   $ "Loop @ " ++ show n0
   where
-    coll :: Integer -> Integer -> Set Integer -> ([Integer], Bool)
-    coll n i set = if n == i then ([n], False) else case DS.member n set of
+    coll :: Integer -> Integer -> HashSet Integer -> ([Integer], Bool)
+    coll n i set = if n == i then ([n], False) else case HS.member n set of
       True -> ([n], True) ; False -> let (is, c) = coll (next n) i set in (n:is, c)
 
     next :: Integer -> Integer
     next n = case even n of False -> 3*n + 1 ; True -> n `div` 2
 
-    recInsert :: Ord a => [a] -> Set a -> Set a
-    recInsert is m = DL.foldr DS.insert m is
+    recInsert :: (Ord a, Hashable a) => [a] -> HashSet a -> HashSet a
+    recInsert is m = DL.foldr HS.insert m is
 
 {-- main thread --}
 
@@ -69,11 +77,11 @@ main = do hSetBuffering stdout NoBuffering -- standard
           exsts2    <- doesFileExist "collatzMap.prog"
           let exsts  = exsts1 && exsts2          
           if exsts then return () -- we assume these files exists. If they don't, we have to create them now
-                   else do encodeFile "collatzMap.prog" (1                 ::     Integer)
-                           encodeFile "collatzMap.dat"  ((DS.fromList [1]) :: Set Integer)
+                   else do encodeFile "collatzMap.prog" (1                 ::         Integer)
+                           encodeFile "collatzMap.dat"  ((HS.fromList [1]) :: HashSet Integer)
 
-          savedSet <- decodeFile "collatzMap.dat"  :: IO (Set Integer)     -- read progress from disk
-          prevsize <- decodeFile "collatzMap.prog" :: IO      Integer
+          savedSet <- decodeFile "collatzMap.dat"  :: IO (HashSet Integer)     -- read progress from disk
+          prevsize <- decodeFile "collatzMap.prog" :: IO          Integer
 
           atomically $ writeTVar results savedSet
           let newSize = prevsize + step
